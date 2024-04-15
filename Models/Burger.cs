8ec4e9d224698b45535685370bff216f0787d1ab
Kamila <kamilasosa99@icloud.com>
2024-04-14 20:16:03 -0500

    Creacion del modelo Burger.cs

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,7 +1,19 @@
-﻿namespace Practica_Kami_Sosa.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica_Kami_Sosa.Models
 {
     public class Burger 
     {
-        //creacion clase burger 
+        //creacion clase burger
+     public int BurgerID { get; set; }
+
+        //Data annotation, nos ayudan a especificar validaciones adicionales a los datos 
+     [Required]
+     public string? Nombre { get; set; }
+     public bool WithCheese{get; set; }
+
+     [Range(0.01, 9999.99)]
+     public decimal precio { get; set; }
+
     }
 }
